11cf3e978887e6da989866ab17cf557f5e0e1daf
Daniel Dorantes <daniel.dorantes@oesglobal.com>
2018-12-18 20:24:59 -0600

    folder structure updates to match current setup

---
+++ Sample/OddMud.SampleGamePlugins/EventPlugins/SpawnManagerPlugin.cs
@@ -0,0 +1,40 @@
+﻿using OddMud.SampleGame;
+using OddMud.SampleGame.Extensions;
+using OddMud.Core.Interfaces;
+using OddMud.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddMud.SampleGamePlugins.EventPlugins
+{
+    public class SpawnManagerPlugin : TickIntervalEventPlugin
+    {
+        public override string Name => nameof(SpawnManagerPlugin);
+        public override int Interval => 5000;
+
+
+        public override void Configure(IGame game, IServiceProvider serviceProvider)
+        {
+            base.Configure(game, serviceProvider);
+            game.Network.Disconnected += Network_Disconnected;
+        }
+        
+        public override async Task IntervalTick(object sender, EventArgs e)
+        {
+            await base.IntervalTick(sender, e);
+
+            var disconnectedPlayers = Game.Players.Where(o=> !Game.Network.Connections.Contains(o.TransportId)).ToList();
+            disconnectedPlayers.ForEach(async (p) => await Game.RemovePlayerAsync(p));
+        }
+
+        private Task Network_Disconnected(object sender, string transportId)
+        {
+            Game.Log(Microsoft.Extensions.Logging.LogLevel.Information, "NetworkDisconnected" + transportId);
+            return Game.RemovePlayerAsync(Game.Players.GetPlayerByTransportId(transportId));
+        }
+
+    }
+}
